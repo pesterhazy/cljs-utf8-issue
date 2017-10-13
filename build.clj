df48321be276021dc38a814c69f90acbce20b04c
Paulus Esterhazy <pesterhazy@gmail.com>
2017-10-13 16:05:22 +0200

    WIP

--- build.clj
+++ build.clj
@@ -1,3 +1,4 @@
 (require 'cljs.build.api)
 
-(cljs.build.api/build "src" {:output-to "out/main.js"})
+(cljs.build.api/build "src" {:output-to "out/main.js"
+                             :optimizations :simple})
